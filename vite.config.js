98dc8d033f4f9afc5bb50e1ef737840a491b0f82
Evgeniy Poloznov <jenyapol@mail.ru>
2024-04-03 01:20:22 +0300

    Админка

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js', 'resources/js/admin.js'],
             refresh: true,
         }),
         vue({
