1fef7765f4991b6f48b3c7db60be477c11166b9d
Evgeniy Poloznov <jenyapol@mail.ru>
2024-07-19 16:26:20 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/yookassa', [\App\Http\Controllers\Profile\PaymentController::class, 'yookassaWebHook'])
+Route::any('/yookassa', [\App\Http\Controllers\Profile\PaymentController::class, 'yookassaWebHook'])
     ->name('yookassaWebHook');
